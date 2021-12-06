7363f3de5772efdf75221629dc2b824e11d7677c
Dan Storey <danstorey901@gmail.com>
2021-12-06 16:03:02 -0500

    stuff is broken but progressing

--- src/app/misc/user/user-create/user-create.component.ts
+++ src/app/misc/user/user-create/user-create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../user.class';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-user-create',
@@ -7,7 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserCreateComponent implements OnInit {
 
-  constructor() { }
+  pageTitle: string = "User Create";
+  user: User = new User();
+  password!: string;
+  passwordCheck!: string;
+
+  constructor(private usr: UserService, private router: Router) { }
+
+  checkPasswords(): void {
+    this.passwordCheck = "";
+    if(this.user.password !== this.password) {
+      this.passwordCheck = "Passwords do not match!";
+    }
+  }
+
+  save(): void {
+    console.log("B4", this.user);
+    this.usr.create(this.user).subscribe(
+      res => {
+        console.log("User created successfully!");
+        this.router.navigateByUrl("/users/list");
+      },
+      err => {
+        console.error(err);
+
+      }
+    )
+  }
+  
 
   ngOnInit(): void {
   }
--- src/app/misc/user/user-detail/user-detail.component.ts
+++ src/app/misc/user/user-detail/user-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { User } from '../user.class';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-user-detail',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDetailComponent implements OnInit {
 
-  constructor() { }
 
+  pageTitle: string = "User Detail";
+  user!: User;
+  verifyDelete: boolean = false;
+
+
+  constructor( private router: Router,
+    private usr: UserService,
+    private route: ActivatedRoute, ) { }
+
+      remove(): void { 
+    this.verifyDelete = !this.verifyDelete; 
+      }
+
+      verify(): void {
+        this.usr.remove(this.user.id).subscribe({
+          next: res => {
+            console.debug("User deleted successfully!");
+            this.router.navigateByUrl("/users/list");
+          }
+        })
+      }
+          
   ngOnInit(): void {
   }
-
 }
