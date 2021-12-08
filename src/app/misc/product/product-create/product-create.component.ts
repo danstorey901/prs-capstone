54e1176e433125f1d10e5778a8b484574bdabfc3
Dan Storey <danstorey901@gmail.com>
2021-12-08 15:36:12 -0500

    product works minus the edit ts file. subscribe syntax needs to be fixed in product create for some reason. thought it worked before?

--- src/app/misc/product/product-create/product-create.component.ts
+++ src/app/misc/product/product-create/product-create.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Vendor } from '../../vendor/vendor.class';
+import { VendorService } from '../../vendor/vendor.service';
+import { Product } from '../product.class';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-create',
@@ -7,9 +12,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCreateComponent implements OnInit {
 
-  constructor() { }
+  product: Product = new Product ();
+  vendors!: Vendor[];
+  password!: string;
+  passwordCheck!: string;
+
+  constructor(
+    private usr: ProductService,
+    private vndr: VendorService,
+    private router: Router,
+    ) { }
+
+    save(): void {
+      this.vndr.list().subscribe({
+        next: res => {
+          console.debug("Vendors:", res);
+          this.vendors = res as Vendor[];
+        }
+      });
+      console.log("B4", this.product);
+      this.usr.create(this.product).subscribe(
+        res => {
+          console.log("Product created successfully!");
+          this.router.navigateByUrl("/products/list");
+        },
+        err => {
+          console.error(err);
+
+        }
+      )
+      }
+
 
   ngOnInit(): void {
+    this.vndr.list().subscribe({
+      next: res => {
+        console.debug("Vendors:", this.vendors);
+        this.vendors = res as Vendor[];
+      }
+    })
   }
-
 }
--- src/app/misc/product/product-detail/product-detail.component.ts
+++ src/app/misc/product/product-detail/product-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../product.class';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
+  product!: Product;
+  verifyDelete: boolean = false;
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(
+    private prd: ProductService,
+    private route: ActivatedRoute,
+    private router: Router
+     ) { }
+
+  remove(): void { 
+    this.verifyDelete = !this.verifyDelete; 
   }
 
+  verify(): void {
+    this.prd.remove(this.product.id).subscribe({
+      next: res => {
+        console.debug("Product deleted successfully!");
+        this.router.navigateByUrl("/products/list");
+      }
+    })
+  }
+
+  ngOnInit(): void {
+    let id = +this.route.snapshot.params["id"];
+    this.prd.get(id).subscribe({
+      next: res => {
+        console.log("Product:", res as Product);
+        this.product = res as Product;
+      }
+    })
+  }
 }
